9b5e5ab8acec8407d44e4f3d2d0bf5a5b6f2b56e
Hknots <hknots99@gmail.com>
2025-10-28 19:53:28 +0100

    test: validate person sync in OtUngdomSyncTest

--- src/test/java/no/fintlabs/model/otungdom/OtUngdomSyncTest.java
+++ src/test/java/no/fintlabs/model/otungdom/OtUngdomSyncTest.java
@@ -59,7 +59,56 @@ class OtUngdomSyncTest {
 
     @Test
     void syncOtUngdomFromVigoToFint() {
-        String expectedOtUngdomResourcesJsonPost = """
+        String expectedOtUngdomResourcesJsonPost = expectedOtUngdomResourcesJsonPost();
+        String expectedPersonResourcesJsonPost = expectedPersonResourcesJsonPost();
+
+        fintProvider.stubFor(post("/provider/utdanning/ot/otungdom")
+                .willReturn(aResponse().withStatus(200)));
+        fintProvider.stubFor(post("/provider/utdanning/ot/person")
+                .willReturn(aResponse().withStatus(200)));
+
+        otUngdomSync.syncOtUngdomFromVigoToFint();
+
+        await().atMost(5, TimeUnit.SECONDS)
+                .untilAsserted(() -> {
+                    vigo.verify(getRequestedFor(urlEqualTo("/"))
+                            .withHeader("fylkesnr", equalTo(vigoFylkesNr))
+                            .withHeader("api-key", equalTo(vigoApiKey))
+                            .withHeader("Accept", equalTo(APPLICATION_JSON.toString())));
+                });
+
+        // Wait for the reactive flow to complete
+        await().atMost(5, TimeUnit.SECONDS)
+                .untilAsserted(() -> {
+                    fintProvider.verify(postRequestedFor(urlEqualTo("/provider/utdanning/ot/otungdom"))
+                            .withRequestBody(matchingJsonPath("$.metadata.adapterId", equalTo(adapterId)))
+                            .withRequestBody(matchingJsonPath("$.metadata.orgId", equalTo("fintlabs.no")))
+                            .withRequestBody(matchingJsonPath("$.metadata.totalSize", equalTo("1")))
+                            .withRequestBody(matchingJsonPath("$.metadata.page", equalTo("1")))
+                            .withRequestBody(matchingJsonPath("$.metadata.pageSize", equalTo("1")))
+                            .withRequestBody(matchingJsonPath("$.metadata.totalPages", equalTo("1")))
+                            .withRequestBody(matchingJsonPath("$.metadata.uriRef", equalTo("/utdanning/ot/otungdom")))
+                            .withRequestBody(matchingJsonPath("$.resources", equalToJson(expectedOtUngdomResourcesJsonPost)))
+                            .withRequestBody(matchingJsonPath("$.syncType", equalTo("FULL"))));
+                });
+
+        await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
+            fintProvider.verify(postRequestedFor(urlEqualTo("/provider/utdanning/ot/person"))
+                    .withRequestBody(matchingJsonPath("$.metadata.adapterId", equalTo(adapterId)))
+                    .withRequestBody(matchingJsonPath("$.metadata.orgId", equalTo("fintlabs.no")))
+                    .withRequestBody(matchingJsonPath("$.metadata.totalSize", equalTo("1")))
+                    .withRequestBody(matchingJsonPath("$.metadata.page", equalTo("1")))
+                    .withRequestBody(matchingJsonPath("$.metadata.pageSize", equalTo("1")))
+                    .withRequestBody(matchingJsonPath("$.metadata.totalPages", equalTo("1")))
+                    .withRequestBody(matchingJsonPath("$.metadata.uriRef", equalTo("/utdanning/ot/person")))
+                    .withRequestBody(matchingJsonPath("$.resources", equalToJson(expectedPersonResourcesJsonPost)))
+                    .withRequestBody(matchingJsonPath("$.syncType", equalTo("FULL")))
+            );
+        });
+    }
+
+    private String expectedOtUngdomResourcesJsonPost() {
+        return """
                 [
                   {
                     "identifier": "0102200312345",
@@ -93,34 +142,38 @@ class OtUngdomSyncTest {
                   }
                 ]
                 """;
-        fintProvider.stubFor(post("/provider/utdanning/ot/otungdom")
-                .willReturn(aResponse().withStatus(200)));
-
-        otUngdomSync.syncOtUngdomFromVigoToFint();
-
-        await()
-                .atMost(5, TimeUnit.SECONDS)
-                .untilAsserted(() -> {
-                    vigo.verify(getRequestedFor(urlEqualTo("/"))
-                            .withHeader("fylkesnr", equalTo(vigoFylkesNr))
-                            .withHeader("api-key", equalTo(vigoApiKey))
-                            .withHeader("Accept", equalTo(APPLICATION_JSON.toString())));
-                });
-
-        // Wait for the reactive flow to complete
-        await()
-                .atMost(5, TimeUnit.SECONDS)
-                .untilAsserted(() -> {
-                    fintProvider.verify(postRequestedFor(urlEqualTo("/provider/utdanning/ot/otungdom"))
-                            .withRequestBody(matchingJsonPath("$.metadata.adapterId", equalTo(adapterId)))
-                            .withRequestBody(matchingJsonPath("$.metadata.orgId", equalTo("fintlabs.no")))
-                            .withRequestBody(matchingJsonPath("$.metadata.totalSize", equalTo("1")))
-                            .withRequestBody(matchingJsonPath("$.metadata.page", equalTo("1")))
-                            .withRequestBody(matchingJsonPath("$.metadata.pageSize", equalTo("1")))
-                            .withRequestBody(matchingJsonPath("$.metadata.totalPages", equalTo("1")))
-                            .withRequestBody(matchingJsonPath("$.metadata.uriRef", equalTo("/utdanning/ot/otungdom")))
-                            .withRequestBody(matchingJsonPath("$.resources", equalToJson(expectedOtUngdomResourcesJsonPost)))
-                            .withRequestBody(matchingJsonPath("$.syncType", equalTo("FULL"))));
-                });
     }
+
+    private String expectedPersonResourcesJsonPost() {
+        return """
+                [
+                  {
+                    "identifier": "0102200312345",
+                    "resource": {
+                      "kontaktinformasjon": {
+                        "epostadresse": "foo.bar.1@test.no",
+                        "mobiltelefonnummer": "11111111"
+                      },
+                      "fodselsdato": "2003-01-31T23:00:00.000+00:00",
+                      "fodselsnummer": {
+                        "identifikatorverdi": "0102200312345"
+                      },
+                      "navn": {
+                        "fornavn": "Foo 1",
+                        "etternavn": "Bar 1"
+                      },
+                      "_links": {
+                        "otungdom": [
+                          { "href": "systemid/0102200312345" }
+                        ],
+                        "self": [
+                          { "href": "fodselsnummer/0102200312345" }
+                        ]
+                      }
+                    }
+                  }
+                ]
+                """;
+    }
+
 }
